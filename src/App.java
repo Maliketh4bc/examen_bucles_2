29a8924bd9530aaa634f389159b85cab56a4bd80
Maliketh4bc <marcoalvar1212@gmail.com>
2025-11-18 09:07:56 +0100

    nivel 2

--- src/App.java
+++ src/App.java
@@ -1,13 +1,125 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        
-        boolean up = true;           // Barra superior
-        boolean upRgt = false;    // Barra superior derecha
-        boolean upLft = true;  // Barra superior derecha
-        boolean center = true;            // Barra central
-        boolean downLft = false;  // Barra inferior izquierda
-        boolean downRgt = true;    // Barra inferior derecha
-        boolean down = true;           // Barra inferior
+
+    static boolean up = false;           // Barra superior
+    static boolean upRgt = false;    // Barra superior derecha
+    static boolean upLft = false;  // Barra superior derecha
+    static boolean center = false;            // Barra central
+    static boolean downLft = false;  // Barra inferior izquierda
+    static boolean downRgt = false;    // Barra inferior derecha
+    static boolean down = false;           // Barra inferior
+
+    static int digit = 0;
+
+    public static void request(){
+        do{
+            System.out.print("Introduce el dígito que quieres imprimir(0-9): ");
+            digit = Integer.parseInt(System.console().readLine());
+            if(digit>9) System.out.println("Introduce numero válido");
+
+        }while(digit>9);
+    }
+
+    public static void setNumber(){
+
+        switch (digit) {
+            case 0:
+                up = true;
+                upLft = true;
+                upRgt = true;
+                center = false;
+                downLft = true;
+                downRgt = true;
+                down = true;
+                break;
+            case 1:
+                up = false;
+                upLft = false;
+                upRgt = true;
+                center = false;
+                downLft = false;
+                downRgt = true;
+                down = false;
+                break;
+            case 2:
+                up = true;
+                upLft = false;
+                upRgt = true;
+                center = true;
+                downLft = true;
+                downRgt = false;
+                down = true;
+                break;
+            case 3:
+                up = true;
+                upLft = false;
+                upRgt = true;
+                center = true;
+                downLft = false;
+                downRgt = true;
+                down = true;
+                break;
+            case 4:
+                up = false;
+                upLft = true;
+                upRgt = true;
+                center = true;
+                downLft = false;
+                downRgt = true;
+                down = false;
+                break;
+            case 5:
+                up = true;
+                upLft = true;
+                upRgt = false;
+                center = true;
+                downLft = false;
+                downRgt = true;
+                down = true;
+                break;
+            case 6:
+                up = false;
+                upLft = true;
+                upRgt = false;
+                center = true;
+                downLft = true;
+                downRgt = true;
+                down = true;
+                break;
+            case 7:
+                up = true;
+                upLft = false;
+                upRgt = true;
+                center = false;
+                downLft = false;
+                downRgt = true;
+                down = false;
+                break;
+            case 8:
+                up = true;
+                upLft = true;
+                upRgt = true;
+                center = true;
+                downLft = true;
+                downRgt = true;
+                down = true;
+                break;
+            case 9:
+                up = true;
+                upLft = true;
+                upRgt = true;
+                center = true;
+                downLft = false;
+                downRgt = true;
+                down = false;
+                break;
+            default:
+                System.out.println("Introduce un numero correcto");
+                break;
+        }
+
+    }
+
+    public static void printNum(){
 
         for(int i=1; i<=7; i++){
             
@@ -18,11 +130,12 @@ public class App {
                 }
 
                 if(upLft == true && (i == 2 || i==3)){
-                    System.out.printf("%s",(j==1)?"*":" ");
+                    System.out.printf("%s",(j==1)?"*":(j==5)?"":" ");
                 }
 
                 if(upRgt == true && (i == 2 || i==3)){
-                    System.out.printf("%s",(j==5)?" ":"*");
+                    if(upLft == false) System.out.printf("%s", (j<5)?" ":"");
+                    System.out.printf("%s",(j==5)?"*":"");
                 }
 
                 if(center == true && i == 4){
@@ -30,11 +143,12 @@ public class App {
                 }
                 
                 if(downLft == true && (i == 5 || i==6)){
-                    System.out.printf("%s",(j==1)?"*":" ");
+                    System.out.printf("%s",(j==1)?"*":(j==5)?"":" ");
                 }
 
                 if(downRgt == true && (i == 5 || i==6)){
-                    System.out.printf("%s",(j==5)?"*":" ");
+                    if(downLft == false) System.out.printf("%s", (j<5)?" ":"");
+                    System.out.printf("%s",(j==5)?"*":"");
                 }
 
                 if(down == true && i == 7){
@@ -46,6 +160,22 @@ public class App {
             System.out.println();
         }
 
+    }
 
+    public static void limpiarPantalla() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+        do{
+            //limpiarPantalla();
+            request();
+
+            setNumber();
+
+            printNum();
+        }while(digit != -1);
     }
 }
